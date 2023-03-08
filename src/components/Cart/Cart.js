2d17799cd333f9f5fb3344bf1ec82831a4574540
asifalam515 <asibulalam515@gmail.com>
2023-03-08 23:43:51 +0600

    total added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const {cart}=props
+   let total=0;
+   let shipping=0;
+   for(const product of cart){
+    total=product.price+total
+    shipping=shipping+product.shipping
+   }
+   const tax=(total*0.1)
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summary</h1>
-        <p>Selected Item:{props.cart.length}</p>
+        <p>Selected Item:{cart.length}</p>
+        <p>Total Price:${total} </p>
+        <p>Total Shipping:${shipping} </p>
+        <p>Tax:${tax.toFixed(2)}</p>
+        <h5>Grand Total:</h5>
             
         </div>
     );
